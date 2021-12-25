f9cfd8824e9901ec77c7304279c91e4e6ba11d5c
Ali Ali <ali.bu3alwa@gmail.com>
2021-12-25 02:36:42 -0500

    trpc seperation of componenets and addition of user route

--- src/pages/api/auth/logout.tsx
+++ src/pages/api/auth/logout.tsx
@@ -1,19 +1,15 @@
-
 import { NextApiRequest, NextApiResponse } from 'next';
-import nc from "next-connect";
-import jwtVerify from '../../../src/middleware/jwt';
-
+import nc from 'next-connect';
+import jwtVerify from '../../../middleware/jwt';
 
 /**
- * Logout handler function 
+ * Logout handler function
  * Logs the user out and returns 204
  */
 const handler = nc<NextApiRequest, NextApiResponse>();
-handler
-.use(jwtVerify)
-  .post((req, res) => {
-    // TODO: implement token invalidation
-    res.status(204).end();
+handler.use(jwtVerify).post((req, res) => {
+  // TODO: implement token invalidation
+  res.status(204).end();
 });
 
 export default handler;
